3195cc72816444d777cbec916c701a9c08f85fbf
Justin Brewer <jzb0012@auburn.edu>
2013-06-01 16:27:30 -0500

    //r

--- d20.py
+++ d20.py
@@ -16,7 +16,7 @@ class D20Bot(JabberBot):
     @botcmd
     def roll(self,mess,args):
         args,comment = (args.split('#',1)+[''])[:2]
-        comment = '\n'+comment if comment != '' else ''
+        comment = ' '+comment if comment != '' else ''
         
         if mess.getFrom().getStripped() in chatrooms:
             name = mess.getFrom().getResource()
@@ -36,6 +36,10 @@ class D20Bot(JabberBot):
         for i in res:
             total += i
         return ""+name+" rolled "+str(total)+" "+repr(res)+comment
+
+    @botcmd
+    def r(self,mess,args):
+        return self.roll(mess,"1d20 "+args)
 
 bot = D20Bot(username,password,**{'command_prefix':'//'})
 
