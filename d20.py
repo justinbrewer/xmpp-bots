1c98c7da708f77ced996d3f1c1bee9e636b8a5bf
Justin Brewer <jzb0012@auburn.edu>
2013-06-01 18:21:01 -0500

    Implement braces

--- d20.py
+++ d20.py
@@ -13,8 +13,7 @@ logging.basicConfig()
 class D20Bot(JabberBot):
     PING_FREQUENCY = 60
 
-    @botcmd
-    def roll(self,mess,args):
+    def rolls(self,mess,args):
         args,comment = (args.split('#',1)+[''])[:2]
         comment = ' '+comment if comment != '' else ''
         
@@ -22,7 +21,18 @@ class D20Bot(JabberBot):
             name = mess.getFrom().getResource()
         else:
             name = mess.getFrom().getNode()
-        
+
+        if args.find('{') > -1:
+            results = []
+
+            start = args.index('{')
+            end = args.index('}')
+
+            seq = args[start+1:end].split(',')
+            for i in seq:
+                results = results + self.rolls(mess,args[:start]+i+args[end+1:])
+            return results
+
         argv = args.split()
         res = []
         for i in argv:
@@ -35,7 +45,23 @@ class D20Bot(JabberBot):
         total = 0
         for i in res:
             total += i
-        return ""+name+" rolled "+str(total)+" "+repr(res)+comment
+        return [""+name+" rolled "+str(total)+" "+repr(res)+comment]
+
+    @botcmd
+    def roll(self,mess,args):
+        res = self.rolls(mess,args)
+        if len(res) > 1:
+            comment = (args.split('#',1)+[''])[1]
+            message = ''
+            count = 1
+            for i in res:
+                message = message+'\n('+str(count)+') '+i
+                count = count + 1
+            if comment != '':
+                message = message + '\n' + comment
+            return message
+        else:
+            return res[0]
 
     @botcmd
     def r(self,mess,args):
@@ -44,14 +70,7 @@ class D20Bot(JabberBot):
     @botcmd
     def sr(self,mess,args):
         args,comment = (args.split('#',1)+[''])[:2]
-	args = args.split(',')
-
-        braced = '{'
-        for i in args:
-            braced = braced + i + ','
-        braced = braced[:-1]+'}'
-
-        return self.r(mess,braced+'#'+comment)
+        return self.r(mess,'{'+','.join(args.split())+'}#'+comment)
 
 bot = D20Bot(username,password,**{'command_prefix':'//'})
 
