b5bb099eae0efe5c9ef2f823dcc35887c06e8a6c
Justin Brewer <jzb0012@auburn.edu>
2015-05-19 01:33:41 -0400

    Bet Game Part 1

--- d20.py
+++ d20.py
@@ -10,13 +10,108 @@ import random
 import logging
 logging.basicConfig()
 
+class BetGame():
+    players = {}
+    bet_order = []
+    bets = {}
+    dealer = 0
+    turn = 0
+
+    def enter(self,name,amount):
+        if name in self.players:
+            return False
+        self.players[name] = amount
+        self.bet_order.append(name)
+        self.bets[name] = 0
+        return True
+
+    def bet(self,name,amount):
+        if not name in self.players:
+            return False
+        if self.bet_order[self.turn] != name:
+            return False
+
+        if self.players[name] > amount:
+            self.players[name] = self.players[name] - amount
+            self.bets[name] = self.bets[name] + amount
+            result = True
+        else:
+            self.bets[name] = self.bets[name] + self.players[name]
+            self.players[name] = 0
+            result = self.bets[name]
+
+        self.turn = (self.turn + 1) % len(self.players)
+        return result
+
+    def win(self,name):
+        if not name in self.players:
+            return False
+
+        entitlement = self.bets[name]
+        amount = 0
+        for k,v in self.bets.items():
+            amount = amount + min(entitlement, v)
+            self.bets[k] = max(v - entitlement, 0)
+        self.players[name] = self.players[name] + amount
+        return amount
+
+    def pot(self):
+        return sum(self.bets.itervalues())
+
 class D20Bot(JabberBot):
     PING_FREQUENCY = 60
+
+    game = None
+
+    @botcmd
+    def begin_game(self,mess,args):
+        if self.game != None:
+            return "A game is already in session. Use /end_game to end first."
+        self.game = BetGame()
+        return None
+
+    @botcmd
+    def end_game(self,mess,args):
+        self.game = None
+        return "Game ended."
+
+    @botcmd
+    def enter(self,mess,args):
+        name = self.who(mess)
+        amount = int(args)
+        if self.game.enter(name, amount):
+            return "" + name + " entered with " + str(amount) + " chips."
+        else:
+            return "" + name + " is already in the game."
+
+    @botcmd
+    def bet(self,mess,args):
+        name = self.who(mess)
+        amount = int(args)
+        result = self.game.bet(name, amount)
+        if result == True:
+            return "" + name + " bet " + str(amount) + " chips."
+        elif result == False:
+            return "It is not your turn to bet."
+        else:
+            return "" + name + " bet " + str(amount) + " chips. They are now all-in for " + str(result) + " chips."
+
+    @botcmd
+    def win(self,mess,args):
+        result = self.game.win(args)
+        if result == False:
+            return "" + args + " is not in the game."
+        else:
+            pot = self.game.pot()
+            potmsg = ""
+            if pot > 0:
+                potmsg = " There are still " + str(pot) + " chips in the pot."
+            return "" + args + " won " + str(result) + " chips." + potmsg
 
     def rolls(self,mess,args):
         args,comment = (args.split('#',1)+[''])[:2]
         comment = ' '+comment if comment != '' else ''
-        
+
         if mess.getFrom().getStripped() in chatrooms:
             name = mess.getFrom().getResource()
         else:
@@ -72,7 +167,13 @@ class D20Bot(JabberBot):
         args,comment = (args.split('#',1)+[''])[:2]
         return self.r(mess,'{'+','.join(args.split())+'}#'+comment)
 
-bot = D20Bot(username,password,**{'command_prefix':'//'})
+    def who(self, mess):
+        if mess.getFrom().getStripped() in chatrooms:
+            return mess.getFrom().getResource()
+        else:
+            return mess.getFrom().getNode()
+
+bot = D20Bot(username,password,**{'command_prefix':'/'})
 
 for i in chatrooms:
 	bot.join_room(i)
